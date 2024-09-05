943a03372a60a01515e0cab16d2f5f7c2d144c0e
Amaljith TM <amaljith.tm@trenser.com>
2024-09-05 21:10:41 +0530

    Day4_optionSelection

---
+++ Selenium/Day4/Pack1/Pgm1.py
@@ -0,0 +1,19 @@
+#Dropdown[Single selection]
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+driver=webdriver.Chrome()
+driver.maximize_window()
+driver.get("file:///E:/Sony/Emexo/Project2_Selenium/sample/BasicHtmlElement.html")
+time.sleep(3)
+state_dropdown=driver.find_element(By.NAME,"state")
+sel=Select(state_dropdown)
+sel.select_by_index(1)
+time.sleep(3)
+sel.select_by_value("2")
+time.sleep(3)
+sel.select_by_visible_text("TELENGANA")
+time.sleep(3)
+print(sel.is_multiple)
+driver.quit()
---
+++ Selenium/Day4/Pack1/Pgm3.py
@@ -0,0 +1,25 @@
+#Select multiple and deselect
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+driver=webdriver.Chrome()
+driver.maximize_window()
+driver.get("file:///E:/Sony/Emexo/Project2_Selenium/sample/BasicHtmlElement.html")
+time.sleep(3)
+state_dropdown=driver.find_element(By.NAME,"alloptions")
+sel=Select(state_dropdown)
+sel.select_by_index(1)
+time.sleep(3)
+sel.select_by_value("4")
+time.sleep(3)
+sel.select_by_visible_text("THREE")
+time.sleep(3)
+print(sel.is_multiple)
+sel.deselect_by_index(1)
+time.sleep(3)
+sel.deselect_by_value("4")
+time.sleep(3)
+sel.deselect_by_visible_text("THREE")
+time.sleep(3)
+driver.quit()
